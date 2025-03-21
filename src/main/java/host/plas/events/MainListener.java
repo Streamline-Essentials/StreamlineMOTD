3b04a1f40e98566d9d2ce9ffb5e2cc2a54e710eb
Matt <matt@plas.host>
2025-03-21 00:52:17 -0400

    update: deps

--- src/main/java/host/plas/events/MainListener.java
+++ src/main/java/host/plas/events/MainListener.java
@@ -1,8 +1,9 @@
 package host.plas.events;
 
-import singularity.events.server.ping.PingReceivedEvent;
+import host.plas.events.own.MOTDSendEvent;
 import host.plas.StreamlineMOTD;
 import host.plas.config.MOTDConfig;
+import singularity.events.server.ping.PingReceivedEvent;
 import tv.quaint.events.BaseEventListener;
 import tv.quaint.events.processing.BaseProcessor;
 
@@ -11,6 +12,11 @@ public class MainListener implements BaseEventListener {
     public void onPing(PingReceivedEvent event) {
         if (StreamlineMOTD.getConfig().getUpdateType() == MOTDConfig.UpdateType.ON_PING) {
             StreamlineMOTD.getConfig().updateResponse();
+
+            MOTDSendEvent sendEvent = new MOTDSendEvent(event).fire();
+            if (sendEvent.isCancelled()) {
+                return;
+            }
         }
 
         event.setResponse(StreamlineMOTD.getConfig().build(event.getResponse()));
